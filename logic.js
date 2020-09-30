506ca9cf58dd8cf137da6ca98c297bfa10759e9d
Ryan Helgerson <ryan.helgerson@gmail.com>
2020-09-30 14:25:07 -0500

    deleting stuff from db in real time.

--- logic.js
+++ logic.js
@@ -9,23 +9,34 @@ function renderEntries(doc){
     let title = document.createElement('span');
     let dry = document.createElement('span');
     let date = document.createElement('span');
+    let deleteBtn = document.createElement('div');
 
     li.setAttribute('data-id', doc.id);
     date.innerHTML = `<p> Enry date: <strong>${doc.data().date}</strong> </p>`;
     title.innerHTML = `<p> How did you feel? <strong>${doc.data().title} </strong> </p>`;
-    dry.innerHTML = `<p>Did you stay dry? <strong>${doc.data().dry}</strong> </p>
-                    <br>`;
+    dry.innerHTML = `<p>Did you stay dry? <strong>${doc.data().dry}</strong> </p>`;
+    deleteBtn.innerHTML = `<button class="delete">Delete Entry</button>`
     
+
     li.appendChild(date);
     li.appendChild(title);
     li.appendChild(dry);
+    li.appendChild(deleteBtn);
 
     entryList.appendChild(li);
+    
+    //deleting data
+    deleteBtn.addEventListener('click', (e) => {
+        //e.stopPropagation();//this may be unnecessary
+        //create a variable that targets the document id that we stored in data-id property for each li
+        let id = e.target.parentElement.parentElement.getAttribute(`data-id`); //brush up on the 'elem.closest' method and refactor this
+        db.collection(`entries`).doc(id).delete();
+    })
 }
 
 //get a reference to the data inside the database
 db.collection(`entries`).get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
+    snapshot.docs.forEach(doc => {//I'm a little unsure as to why the syntax here works. I'll have to look up the .then method, but what is 'docs'?
         renderEntries(doc);
     })
 });
